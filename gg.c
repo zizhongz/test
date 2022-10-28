89f4aaf60e81315018abcb60092a969a8ce2b26a
Zizhong Zhu <zizhong.zhu@corigine.com>
2022-10-28 17:26:42 +0800

    new branch 11111111111111
    main 11111111111
    
    main 222222222222
    
    new branch 2222222222222

--- gg.c
+++ gg.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main(void) { 
-	printf("test1123333");
+	// main 111111111111111111111111111
+	// main 222222222222222222222222222222
+	printf("test11233sss");
 	return 0;
-
-}
+	// zheshi xin de 
+	//newnwewnewnewnew 11111111111111
+	////newnwewnewnewnew 22222222222222222
+}     
